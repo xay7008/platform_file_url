53173ed3fc394f767f20c513e7dfcef988078a3a
xay <xay7008@gmail.com>
2022-04-30 02:00:27 +0800

    add win

--- lib/platform_file_url.dart
+++ lib/platform_file_url.dart
@@ -1,13 +1,62 @@
-
 import 'dart:async';
+import 'dart:io';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 
-class PlatformFileUrl {
-  static const MethodChannel _channel = MethodChannel('platform_file_url');
+import 'platform_file_url.dart';
 
-  static Future<String?> get platformVersion async {
-    final String? version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
+class PlatformFileUrl {
+  static const MethodChannel _channel = MethodChannel(
+      'com.xay/platform_file_url');
+  bool isInitOk = false;
+  OnUrlLinten? onUrlLinten;
+  static final PlatformFileUrl _singleton = new PlatformFileUrl._internal();
+
+  factory PlatformFileUrl() {
+    return _singleton;
+  }
+
+
+  PlatformFileUrl._internal();
+
+  init() {
+    isInitOk = true;
+    _channel.setMethodCallHandler((call) async {
+      if (kDebugMode) {
+        print(
+            "platform_file_url:call.method:${call.method} call arguments:${call
+                .arguments}");
+      }
+      if (call.method == "openFile") {
+        if (Platform.isAndroid || Platform.isMacOS || Platform.isIOS) {
+          openPlatformFile(call.arguments["url"]);
+        } else if (Platform.isWindows) {
+          openPlatformFile(call.arguments);
+        }
+      }
+    });
+  }
+
+  void addUrlLinten(OnUrlLinten onUrlLinten) {
+    this.onUrlLinten = onUrlLinten;
+  }
+
+  Future<void> openPlatformFile(String? url) async {
+    if (!isInitOk) {
+      if (kDebugMode) {
+        print("platform_file_url:openPlatformFile not init");
+      }
+      return;
+    }
+    if (url == null || url.isEmpty) {
+      if (kDebugMode) {
+        print("platform_file_url:openPlatformFile url is null");
+      }
+      return;
+    }
+    onUrlLinten?.call(url);
   }
 }
+
+typedef OnUrlLinten = Function(String url);
--- windows/platform_file_url_plugin.cpp
+++ windows/platform_file_url_plugin.cpp
@@ -1,77 +1,77 @@
+#pragma once
+
 #include "include/platform_file_url/platform_file_url_plugin.h"
 
 // This must be included before many other Windows headers.
-#include <windows.h>
-
-// For getPlatformVersion; remove unless needed for your plugin implementation.
-#include <VersionHelpers.h>
-
 #include <flutter/method_channel.h>
 #include <flutter/plugin_registrar_windows.h>
 #include <flutter/standard_method_codec.h>
-
-#include <map>
-#include <memory>
-#include <sstream>
 
 namespace {
 
 class PlatformFileUrlPlugin : public flutter::Plugin {
  public:
-  static void RegisterWithRegistrar(flutter::PluginRegistrarWindows *registrar);
+  static void RegisterWithRegistrar(flutter::PluginRegistrarWindows* registrar);
 
-  PlatformFileUrlPlugin();
+  PlatformFileUrlPlugin(flutter::PluginRegistrarWindows* registrar);
 
   virtual ~PlatformFileUrlPlugin();
 
  private:
-  // Called when a method is called on this plugin's channel from Dart.
   void HandleMethodCall(
-      const flutter::MethodCall<flutter::EncodableValue> &method_call,
+      const flutter::MethodCall<flutter::EncodableValue>& method_call,
       std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result);
+
+  std::string GetErrorString(std::string method_name);
+
+  flutter::PluginRegistrarWindows* registrar_;
+  std::unique_ptr<flutter::MethodChannel<flutter::EncodableValue>> channel_;
 };
 
-// static
 void PlatformFileUrlPlugin::RegisterWithRegistrar(
-    flutter::PluginRegistrarWindows *registrar) {
-  auto channel =
-      std::make_unique<flutter::MethodChannel<flutter::EncodableValue>>(
-          registrar->messenger(), "platform_file_url",
-          &flutter::StandardMethodCodec::GetInstance());
-
-  auto plugin = std::make_unique<PlatformFileUrlPlugin>();
-
-  channel->SetMethodCallHandler(
-      [plugin_pointer = plugin.get()](const auto &call, auto result) {
-        plugin_pointer->HandleMethodCall(call, std::move(result));
-      });
-
+    flutter::PluginRegistrarWindows* registrar) {
+  auto plugin = std::make_unique<PlatformFileUrlPlugin>(registrar);
   registrar->AddPlugin(std::move(plugin));
 }
 
-PlatformFileUrlPlugin::PlatformFileUrlPlugin() {}
+PlatformFileUrlPlugin::PlatformFileUrlPlugin(
+    flutter::PluginRegistrarWindows* registrar)
+    : registrar_(registrar) {
+  channel_ = std::make_unique<flutter::MethodChannel<flutter::EncodableValue>>(
+      registrar_->messenger(), "com.xay/platform_file_url",
+      &flutter::StandardMethodCodec::GetInstance());
+  channel_->SetMethodCallHandler([this](const auto& call, auto result) {
+    HandleMethodCall(call, std::move(result));
+  });
+  registrar_->RegisterTopLevelWindowProcDelegate([=](HWND hwnd, UINT message,
+                                                     WPARAM wparam,
+                                                     LPARAM lparam)
+                                                     -> std::optional<HRESULT> {
+    {
+      switch (message) {
+        case WM_COPYDATA: {
+          channel_->InvokeMethod(
+              //std::string(),
+              "openFile",
+              std::make_unique<flutter::EncodableValue>(
+                  flutter::EncodableValue(std::string(static_cast<char*>(
+                      reinterpret_cast<COPYDATASTRUCT*>(lparam)->lpData)))));
+        }
+        default:
+          break;
+      }
+      return std::nullopt;
+    }
+  });
+}
 
 PlatformFileUrlPlugin::~PlatformFileUrlPlugin() {}
 
 void PlatformFileUrlPlugin::HandleMethodCall(
-    const flutter::MethodCall<flutter::EncodableValue> &method_call,
+    const flutter::MethodCall<flutter::EncodableValue>& method_call,
     std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result) {
-  if (method_call.method_name().compare("getPlatformVersion") == 0) {
-    std::ostringstream version_stream;
-    version_stream << "Windows ";
-    if (IsWindows10OrGreater()) {
-      version_stream << "10+";
-    } else if (IsWindows8OrGreater()) {
-      version_stream << "8";
-    } else if (IsWindows7OrGreater()) {
-      version_stream << "7";
-    }
-    result->Success(flutter::EncodableValue(version_stream.str()));
-  } else {
-    result->NotImplemented();
-  }
+  result->NotImplemented();
 }
-
 }  // namespace
 
 void PlatformFileUrlPluginRegisterWithRegistrar(
